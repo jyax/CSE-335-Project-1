40c51c61c503da516ee434d2307e8a9110eb5733
Nicole Kuang <kuangnic@msu.edu>
2023-02-26 14:04:57 -0500

    Added necessary future code to ids.h

--- GameLib/ids.h
+++ GameLib/ids.h
@@ -8,4 +8,13 @@
 #ifndef PROJECT1_GAMELIB_IDS_H
 #define PROJECT1_GAMELIB_IDS_H
 
+/// IDs for levels in the game
+
+// uncomment when ready to be used
+/*enum IDs {
+	IDM_LEVELZERO = wxID_HIGHEST + 1,
+	IDM_LEVELONE,
+	IDM_LEVELTWO,
+	IDM_LEVELTHREE
+};*/
 #endif //PROJECT1_GAMELIB_IDS_H
