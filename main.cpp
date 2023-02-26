aef5bdd9c9dd803d66a19538340df3db082d0143
Nicole Kuang <kuangnic@msu.edu>
2023-02-26 13:56:58 -0500

    Added comments and necessary future code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
+/**
+ * @file main.cpp
+ * @author Nicole Kuang
+ *
+ * Main entry point for the game
+ */
+
+#include "pch.h"
 #include <iostream>
 
+//wxIMPLEMENT_APP(GameApp);
+
+// delete the following when GameApp is created
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
