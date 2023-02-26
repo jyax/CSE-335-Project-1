4b33ab0dd3edb312a0b7438b932424d773748c6a
Nicole Kuang <kuangnic@msu.edu>
2023-02-26 13:58:57 -0500

    Added ids.h

---
+++ GameLib/ids.h
@@ -0,0 +1,11 @@
+/**
+ * @file ids.h
+ * @author Nicole Kuang
+ *
+ * ID values for the game
+ */
+
+#ifndef PROJECT1_GAMELIB_IDS_H
+#define PROJECT1_GAMELIB_IDS_H
+
+#endif //PROJECT1_GAMELIB_IDS_H
