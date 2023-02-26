432a62b9aa95b2cd83d7ec4d4a2443bd08679989
jyax <86649500+jyax@users.noreply.github.com>
2023-02-26 13:23:48 -0500

    Added Game Library

---
+++ Tests/EmptyTest.cpp
@@ -0,0 +1,10 @@
+#include <pch.h>
+#include "gtest/gtest.h"
+
+
+
+TEST(EmptyTest, ExampleTest){
+
+
+  ASSERT_EQ(7, 7);
+}
---
+++ Tests/gtest_main.cpp
@@ -0,0 +1,12 @@
+#include <pch.h>
+#include "gtest/gtest.h"
+#include <wx/filefn.h>
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+
+    wxSetWorkingDirectory(L"..");
+    wxInitAllImageHandlers();
+
+    return RUN_ALL_TESTS();
+}
