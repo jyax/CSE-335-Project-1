4fa6d8336adf37a210e24a3371fb2f1c5f835ab5
Nicole Kuang <kuangnic@msu.edu>
2023-02-28 17:50:30 -0500

    Added GameTest.cpp to Tests and a test for constructing Game object

---
+++ Tests/GameTest.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file GameTest.cpp
+ * @author Nicole Kuang
+ */
+
+#include <pch.h>
+#include "gtest/gtest.h"
+#include <Game.h>
+
+using namespace std;
+
+// Tests if a game can be created
+TEST(GameTest, Construct)
+{
+	Game game;
+}
