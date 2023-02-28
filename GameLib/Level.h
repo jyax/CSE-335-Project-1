5b51c887b409ad17e587ab19e2747d2e86276d1f
Jacob Meier <meierjac@msu.edu>
2023-02-28 18:22:05 -0500

    Added includes and comments

--- GameLib/Level.h
+++ GameLib/Level.h
@@ -2,12 +2,15 @@
  * @file Level.h
  * @author Jacob Meier
  *
- *
+ * Main class for level0, level1, and level2
  */
 
 #ifndef PROJECT1_GAMELIB_LEVEL_H
 #define PROJECT1_GAMELIB_LEVEL_H
 
+/**
+ * Main class for level0, level1, and level2
+ */
 class Level
 {
 private:
