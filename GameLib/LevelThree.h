faab0d857cc0cf1c052d7d83805c2b3c6d0ef7cc
Nicole Kuang <kuangnic@msu.edu>
2023-02-28 17:43:14 -0500

    Added comments and disabled default, copy constructors in LevelThree.h

--- GameLib/LevelThree.h
+++ GameLib/LevelThree.h
@@ -3,20 +3,32 @@
  * @author Nicole Kuang
  *
  * Class that describes Level 3 of the Bug Squash Game
+ *
+ * This class includes special attributes and operations
+ * that are only in Level 3. Delete this class if there are none.
  */
 
 #ifndef PROJECT1_GAMELIB_LEVELTHREE_H
 #define PROJECT1_GAMELIB_LEVELTHREE_H
 
+// inherit from Level
+//#include "Game.h"
+
 /**
  * Class that describes Level 3 of the Bug Squash Game
  */
-class LevelThree
-{
+class LevelThree {
 private:
 
 public:
+	/// Default constructor (disabled)
+	LevelThree() = delete;
 
+	/// Copy constructor (disabled)
+	LevelThree(const LevelThree &) = delete;
+
+	/// Assignment operator (disabled)
+	void operator=(const LevelThree &) = delete;
 };
 
 #endif //PROJECT1_GAMELIB_LEVELTHREE_H
