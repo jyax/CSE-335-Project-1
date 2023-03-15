9c8fa4d2ad7b946b0c87dddcd1bf123ad15ff1f4
Nicole Kuang <kuangnic@msu.edu>
2023-03-15 13:15:12 -0400

    Added Visit functions for each bug type

--- GameLib/ItemVisitor.h
+++ GameLib/ItemVisitor.h
@@ -8,6 +8,11 @@
 #ifndef PROJECT1_GAMELIB_ITEMVISITOR_H
 #define PROJECT1_GAMELIB_ITEMVISITOR_H
 
+class GarbageBug;
+class NullBug;
+class RedundancyBug;
+class Feature;
+
 /**
  * Game item visitor base class
  */
@@ -18,6 +23,30 @@ private:
 public:
 	/** Destructor */
 	virtual ~ItemVisitor() {}
+
+	/**
+	 * Visit a GarbageBug object
+	 * @param bug garbage bug to visit
+	 */
+	virtual void VisitGarbage(GarbageBug *bug) {}
+
+	/**
+	 * Visit a NullBug object
+	 * @param bug null bug to visit
+	 */
+	virtual void VisitNull(NullBug *bug) {}
+
+	/**
+	 * Visit a RedundancyBug object
+	 * @param bug redundancy bug to visit
+	 */
+	virtual void VisitRedundancy(RedundancyBug *bug) {}
+
+	/**
+	 * Visit a Feature object
+	 * @param feature feature 'bug' to visit
+	 */
+	virtual void VisitFeature(Feature *feature) {}
 
 protected:
 	/** Constructor */
