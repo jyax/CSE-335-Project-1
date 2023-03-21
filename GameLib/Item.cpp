ecd6cb06514930b08f19a3c80fe8814af691e03a
Nicole Kuang <kuangnic@msu.edu>
2023-03-21 19:02:52 -0400

    Removed Item::HitTest() body

--- GameLib/Item.cpp
+++ GameLib/Item.cpp
@@ -37,30 +37,11 @@ Item::~Item()
  * Test to see if we hit this object with a mouse.
  * @param x X position to test
  * @param y Y position to test
- * @return true if hit.
+ * @return false
  */
 bool Item::HitTest(double x, double y)
 {
-	double wid = mItemImage->GetWidth();
-	double hit = mItemImage->GetHeight();
-
-	// Make x and y relative to the top-left corner of the bitmap image
-	// Subtracting the center makes x, y relative to the image center
-	// Adding half the size makes x, y relative to the image top corner
-	double testX = x - GetX() + wid / 2;
-	double testY = y - GetY() + hit / 2;
-
-	// Test to see if x, y are in the image
-	if (testX < 0 || testY < 0 || testX >= wid || testY >= hit)
-	{
-		// We are outside the image
-		return false;
-	}
-
-	// Test to see if x, y are in the drawn part of the image
-	// If the location is transparent, we are not in the drawn
-	// part of the image
-	return !mItemImage->IsTransparent((int)testX, (int)testY);
+	return false;
 }
 
 /**
