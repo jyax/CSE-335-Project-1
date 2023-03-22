2a9423a20ababe19186177fd04db3ec340e93ee3
Nicole Kuang <kuangnic@msu.edu>
2023-03-22 17:21:26 -0400

    Added ItemOrder visitor to fix mItems display order

---
+++ GameLib/ItemOrder.h
@@ -0,0 +1,33 @@
+/**
+ * @file ItemOrder.h
+ * @author Nicole Kuang
+ *
+ * Concrete visitor to correct the mItems display order
+ */
+
+#ifndef PROJECT1_GAMELIB_ITEMORDER_H
+#define PROJECT1_GAMELIB_ITEMORDER_H
+
+#include "ItemVisitor.h"
+#include "Item.h"
+
+/**
+ * Concrete visitor to correct the mItems display order
+ */
+class ItemOrder : public ItemVisitor {
+private:
+	/// Temporary list for all programs in a level
+	std::vector<std::shared_ptr<Item>> mPrograms;
+
+	/// Temporary list for all bugs in a level
+	std::vector<std::shared_ptr<Item>> mBugs;
+
+public:
+	void VisitGarbage(GarbageBug* bug) override;
+	void VisitNull(NullBug* bug) override;
+	void VisitRedundancy(RedundancyBug* bug) override;
+	void VisitFeature(Feature* feature) override;
+	void VisitProgram(Program* program) override;
+};
+
+#endif //PROJECT1_GAMELIB_ITEMORDER_H
